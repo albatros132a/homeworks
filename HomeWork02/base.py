7ae62174a3b00933ed89d4548ecec911f23ddb2b
albatros132a <102478963+albatros132a@users.noreply.github.com>
2022-04-10 12:11:02 +0300

    initial commit

--- HomeWork02/base.py
+++ HomeWork02/base.py
@@ -1,55 +1,58 @@
-from abc import ABC
+'''
 
-
-class Vehicle(ABC):
-    pass
-
+доработайте базовый класс base.Vehicle:
+добавьте атрибуты weight, started, fuel, fuel_consumption со значениями по умолчанию
+добавьте инициализатор для установки weight, fuel, fuel_consumption
+добавьте метод start, который, если ещё не состояние started, проверяет, что топлива больше нуля,
+ и обновляет состояние started, иначе выкидывает исключение exceptions.LowFuelError
+добавьте метод move, который проверяет, что достаточно топлива для преодоления переданной дистанции,
+и изменяет количество оставшегося топлива, иначе выкидывает исключение exceptions.NotEnoughFuel
+'''
 
 from abc import ABC
 
 from HomeWork_02.exceptions import exceptions
 
-
-
 class Vehicle(ABC):
-  ''''  weight=2500
+    weight=2500
     started=True
-    fuel=100%
+    fuel=100
     fuel_consumption=9.5
-    distance
-   '''
+    distance=55
+
     pass
 
-    def __init__(self,weight, fuel, fuel_consumption)
+    def __init__(self,weight, fuel, fuel_consumption):
         self.weigh=weight
         self.fuel=fuel
         self.fuel_consumption=fuel_consumption
 
+   # @property
+    #def move(self):
+     #   return fuel=self.fuel
+
+
+
     def start(self):
         if self.started==True:
             return self.started
-        else check_fuel
+        else:
+            return check_fuel()
 
-'''
-try:
-self.started==True
-else:
-
-
-'''
 
 
     def check_fuel(self):
-            if fuel > 0:
-                return True
-            else raise exceptions.LowFuelError()
+        if self.fuel > 0:
+            return True
+        else:
+            raise exceptions.LowFuelError()
 
+    @property
     def move(self,fuel):
-        self.fuel
         move_fuel=distance/100*fuel_consumption
         if (self.fuel-move_fuel)>0:
-            return self.fuel=self.fuel-move_fuel
+            return self.fuel=(self.fuel-move_fuel)
         else:
             raise exceptions.NotEnoughFuel()
 
-car_1=Vehicle(2500,100,9.5)
+car_1=Vehicle()
