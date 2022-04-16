287b7c7e0e1fb76b925047aef1f7c6fe250a0eee
albatros132a <102478963+albatros132a@users.noreply.github.com>
2022-04-16 17:39:39 +0300

    commit after upgrade

---
+++ homework_02/base.py
@@ -0,0 +1,50 @@
+from homework_02.exceptions import LowFuelError, NotEnoughFuel
+
+from abc import ABC
+
+
+class Vehicle(ABC):
+    weight = 2500
+    started = True
+    fuel = 70
+    fuel_consumption = 9.5
+    distance = 55
+
+    def __init__(self, weight, fuel, fuel_consumption):
+        self.weigh = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    def start(self):
+
+        if self.started == True:
+            return self.started
+        else:
+            return check_fuel
+
+    def check_fuel(self):
+
+        if self.fuel > 0:
+            return True
+        else:
+            raise LowFuelError(self.fuel)
+
+    def move(self):
+
+        move_fuel = self.distance / 100 * fuel_consumption
+        if (self.fuel - move_fuel) > 0:
+            self.fuel = (self.fuel - move_fuel)
+
+        else:
+            raise NotEnoughFuel(self.fuel)
+
+
+def main():
+    car_1 = Vehicle(2500, 100, 9.5)
+
+try:
+    main()
+except LowFuelError as e:
+    print(f'Low fuel Error !{e}')
+except NotEnoughFuel as e:
+    print(f'Not enough fuel Error !{e}')
---
+++ homework_02/car.py
@@ -0,0 +1,16 @@
+
+
+from homework_02.base import Vehicle
+
+class Car(Vehicle):
+    pass
+
+    def __init__(self, engine):
+     #   super().__init__())
+        self.engine=engine
+
+    def set_engine(self,engine):
+        return self.engine
+
+car=Car(2.0)
+car.set_engine(2.0)
