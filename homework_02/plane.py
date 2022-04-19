6765a929df198cbf82ac3a4c3bd8bf16aad4a7db
albatros132a <102478963+albatros132a@users.noreply.github.com>
2022-04-19 21:59:55 +0300

    initial commit

---
+++ homework_02/plane.py
@@ -0,0 +1,33 @@
+
+from homework_02.exceptions import CargoOverload
+from homework_02.base import Vehicle
+
+
+class Plane(Vehicle):
+    default_cargo = 1000
+    cargo = 1000
+    max_cargo = 3000
+
+    def __init__(self, cargo, max_cargo):
+      #  super().__init__()
+        self.max_cargo = max_cargo
+        self.cargo = cargo
+
+    def load_cargo(self, cargo):
+        try:
+            if (self.cargo + cargo) < self.max_cargo:
+                self.cargo += cargo
+        except CargoOverload as error:
+            print(f'Cargo ever load Error !{error}')
+
+    def remove_all_cargo(self):
+        self.cargo = self.default_cargo
+
+
+def main():
+    plane=Plane(1000,3000)
+
+try:
+    main()
+except CargoOverload as error:
+    print(f'Low fuel Error !{error}')
